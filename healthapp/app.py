b205ecdd2660b234ce0ce43c3fd4c652a0c38aef
jack <jwd44@cam.ac.uk>
2018-01-20 22:21:25 +0000

    Added description insertion to app.py

--- healthapp/app.py
+++ healthapp/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from healthapp.loader import *
+from healthapp.bin.explanation_getter import insert_descriptions
 
 
 app = Flask(__name__)
@@ -11,5 +12,5 @@ def index():
 @app.route('/record/<id>')
 def display_record(id):
     f = open('../data/{}.json'.format(id), 'r')
-    return compressJson(f.read())
+    return insert_descriptions(compressJson(f.read()))
 
