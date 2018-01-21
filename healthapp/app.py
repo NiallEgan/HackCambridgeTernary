8f99c9cbdf7912e4d72ff78cf69af46c74a9fcd2
NiallEgan <negan1998@gmail.com>
2018-01-21 00:40:24 +0000

    Added explanations to json

--- healthapp/app.py
+++ healthapp/app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from healthapp.loader import *
-
+from healthapp.bin.explanation_getter import insert_descriptions
+import json
 
 app = Flask(__name__)
+
+
+def add_descriptions(compressed_json):
+    return json.dumps({'main_data': json.loads(compressed_json), 
+                       'explanations': json.loads(insert_descriptions(compressed_json))}) 
 
 @app.route('/')
 def index():
@@ -11,5 +17,5 @@ def index():
 @app.route('/record/<id>')
 def display_record(id):
     f = open('../data/{}.json'.format(id), 'r')
-    return compressJson(f.read())
+    return add_descriptions(compressJson(f.read()))
 
